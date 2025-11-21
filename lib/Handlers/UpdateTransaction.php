3d4b34368d5d639ab002fddaa1aeeffd9dec243f
Alex Standiford <alex@standiford.us>
2025-11-21 11:50:11 -0500

    Adds support for transaction creation

--- lib/Handlers/UpdateTransaction.php
+++ lib/Handlers/UpdateTransaction.php
@@ -2,18 +2,68 @@
 
 namespace Novatorius\Siren\ManualAttribution\Handlers;
 
-use NorthCommerceStripe\Issuing\Transaction;
 use PHPNomad\Events\Interfaces\CanHandle;
 use PHPNomad\Events\Interfaces\Event;
+use PHPNomad\Events\Interfaces\EventStrategy;
+use PHPNomad\Template\Interfaces\ScreenResolverStrategy;
+use PHPNomad\Utils\Helpers\Arr;
+use Siren\Commerce\Adapters\FloatToIntPriceAdapter;
+use Siren\Transactions\Core\Models\Transaction;
+use Siren\Transactions\Core\Services\TransactionCreateService;
+use Siren\WordPress\Core\Events\SingleActionCompleted;
 use Siren\WordPress\Core\Events\SingleActionInitiated;
+use Siren\WordPress\Core\Providers\AdminNoticeProvider;
 
 class UpdateTransaction implements CanHandle
 {
+    public function __construct(
+        protected TransactionCreateService $transactionCreateService,
+        protected FloatToIntPriceAdapter $priceAdapter,
+        protected AdminNoticeProvider $notices,
+        protected EventStrategy $event,
+        protected ScreenResolverStrategy $screenResolver
+    )
+    {
+
+    }
+
+    protected function prepareDetails(array $details)
+    {
+        return Arr::map($details, function($detail){
+            $detail['value'] = $this->priceAdapter->toInt(Arr::get($detail, 'value'));
+            settype($detail['quantity'], 'int');
+
+            if($detail['type'] === 'custom'){
+                $detail['type'] = $detail['custom_type'];
+            }
+
+            unset($detail['custom_type']);
+
+            return $detail;
+        });
+    }
+
     public function handle(Event $event): void
     {
         if ($event instanceof SingleActionInitiated && $event->getModel() === Transaction::class) {
-            // TODO: Implement the logic to update the transaction based on the event data.
-            // @see: Siren\WordPress\Integration\Listeners\UpdateProgram For example implementation.
+            $details = $this->prepareDetails(Arr::get($_POST, 'details', []));
+
+            if(empty($details)){
+                return;
+            }
+
+            $transaction = $this->transactionCreateService->maybeCreateTransaction($details);
+
+            if(!$transaction){
+                $this->notices->addNotice("Failed to create transaction.", 'error');
+                wp_redirect($this->screenResolver->getUrlForSlug('siren_transactions', ['siren_notices' => json_encode($this->notices->getNotices())]));
+                return;
+            }
+            $event->setItemId($transaction->getId());
+            $this->notices->addNotice("Transaction created successfully.", 'success');
+
+            $this->event->broadcast(new SingleActionCompleted($event->getItemId(), $event->getAction(), $event->getModel()));
+            wp_redirect($this->screenResolver->getUrlForSlug('siren_transactions', ['siren_notices' => json_encode($this->notices->getNotices())]));
         }
     }
 }
