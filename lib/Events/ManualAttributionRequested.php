a350166675a2ec78ec76aedad45f8dd96bd30dbf
Alex Standiford <alex@standiford.us>
2025-11-22 11:31:19 -0500

    Implements handler (#5)

--- lib/Events/ManualAttributionRequested.php
+++ lib/Events/ManualAttributionRequested.php
@@ -3,9 +3,20 @@
 namespace Novatorius\Siren\ManualAttribution\Events;
 
 use PHPNomad\Events\Interfaces\Event;
+use Siren\Collaborators\Core\Models\Collaborator;
+use Siren\Opportunities\Core\Models\Opportunity;
+use Siren\Transactions\Core\Models\Transaction;
 
 class ManualAttributionRequested implements Event
 {
+    public function __construct(
+       public readonly Collaborator $collaborator,
+       public readonly Transaction $transaction,
+       public readonly Opportunity $opportunity,
+    )
+    {
+
+    }
     public static function getId(): string
     {
         return 'manual_attribution_requested';
--- lib/Strategies/ManualEngagementTriggerStrategy.php
+++ lib/Strategies/ManualEngagementTriggerStrategy.php
@@ -4,10 +4,22 @@ namespace Novatorius\Siren\ManualAttribution\Strategies;
 
 use Novatorius\Siren\ManualAttribution\Events\ManualAttributionRequested;
 use PHPNomad\Events\Interfaces\Event;
+use PHPNomad\Utils\Helpers\Arr;
+use Siren\Collaborators\Core\Models\Program;
 use Siren\Engagements\Core\Interfaces\EngagementTriggerStrategy;
+use Siren\Engagements\Core\Services\CollaboratorActiveProgramService;
+use Siren\Engagements\Core\Services\EngagementTriggerService;
 
 class ManualEngagementTriggerStrategy implements EngagementTriggerStrategy
 {
+    public function __construct(
+        protected CollaboratorActiveProgramService $activeProgramService,
+        protected EngagementTriggerService         $engagementTriggerService
+    )
+    {
+
+    }
+
     public function getName(): string
     {
         return 'Manual Attribution';
@@ -25,7 +37,18 @@ class ManualEngagementTriggerStrategy implements EngagementTriggerStrategy
 
     public function maybeCreateOrUpdateEngagements(Event $event): array
     {
-        // TODO: Implement maybeCreateOrUpdateEngagements() method.
+        if (!$event instanceof ManualAttributionRequested) {
+            return [];
+        }
+
+        $programs = $this->activeProgramService->getActiveProgramIds($this->getId(), $event->collaborator->getId());
+
+        return Arr::map(
+            $programs,
+            fn(Program $program) => $this->engagementTriggerService->triggerEngagement(
+                $this->getId(), $program, $event->collaborator, $event->opportunity
+            )
+        );
     }
 
     public static function getId(): string
